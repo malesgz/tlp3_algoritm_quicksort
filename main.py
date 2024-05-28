4d7fba06ce871098bc29d2906119b08cafab624f
malena <malenagonzalez959@gmail.com>
2024-05-28 16:16:44 -0300

    Ejercicio terminado

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+# Función que toma como argumento una lista.
 def quicksort(array):
+    # Se realiza verifica si la lista contiene menos de 1 elemento para devolverla en su estado orgininal.
     if len(array) <= 1:
         return array
     
+    # Se selecciona como pivote al primer elemento de la lista.
     pivot = array[0]
-    
+
+    # Dos listas que contienen los elementos mayores y menores e iguales al pivote.
     left = [x for x in array[1:] if x < pivot]
     right = [x for x in array[1:] if x >= pivot]
 
+    # Llamada recursiva de ambas listas de forma concatenada con el pivote para que al devolver el resultado se encuentre en el medio. 
     return quicksort(left) + [pivot] + quicksort(right)
 
 # Prueba 1: Lista ordenada
